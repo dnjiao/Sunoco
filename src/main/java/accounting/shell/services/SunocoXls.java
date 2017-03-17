8773b9438f59decb298299ae552b82482bee1a62
dnjiao <oscarjiao@gmail.com>
2017-03-16 23:05:58 -0500

    Enhance settlement matching
    Remove the epsilon for settlement price

--- src/main/java/accounting/shell/services/SunocoXls.java
+++ src/main/java/accounting/shell/services/SunocoXls.java
@@ -41,13 +41,13 @@ public class SunocoXls {
 				coeff = 1;
 				bblCol = 2;
 			} else {
-				break;
+				continue;
 			}	
 			
 			double volEps = 0.05;
-			double priceEps = 5.0;
 			int rowId;
 			Map<Integer, Boolean> volMap = getVolMap(sheet, bblCol);
+			boolean[] mapped = new boolean[settlements.size()];
 			for (rowId = 6; rowId <= sheet.getLastRowNum(); rowId++)
 	    	{
 				Row row = sheet.getRow(rowId);
@@ -59,49 +59,68 @@ public class SunocoXls {
 				if (row.getCell(bblCol) == null || row.getCell(bblCol + 1) == null) {
 					continue;
 				}
+				
 				double volume = row.getCell(bblCol).getNumericCellValue();
 				double price = row.getCell(bblCol + 1).getNumericCellValue();
-				Iterator<Settlement> iter = settlements.iterator();
-				while (iter.hasNext()) {
-					Settlement s = iter.next();
+				
+				
+				double minDelta = Double.MAX_VALUE;
+				int minIndex = -1;
+				for (int j = 0; j < settlements.size(); j++) {
+					if (mapped[j] == true) continue;
+					
+					Settlement s = settlements.get(j);
 					double shellVolume = s.getVolume();
 					double shellPrice = s.getSettleAmount();
+					
+					// only one volume entry
 					if (volMap.get(rowId)) {
 						if (Math.abs(shellVolume - volume) < volEps) {
 							row.getCell(bblCol + 4).setCellValue(shellVolume);
 							row.getCell(bblCol + 5).setCellValue(shellPrice * coeff);
 							double base = shellPrice * coeff / shellVolume;
 							row.getCell(bblCol + 6).setCellValue(base);
-							iter.remove();
+							mapped[j] = true;
 							break;
 						}
 					}
+					// multiple volume entries
 					else {
-						if (Math.abs(shellVolume - volume) < volEps && Math.abs(coeff * shellPrice - price) < priceEps) {
-							row.getCell(bblCol + 4).setCellValue(shellVolume);
-							row.getCell(bblCol + 5).setCellValue(shellPrice * coeff);
-							double base = shellPrice * coeff / shellVolume;
-							row.getCell(bblCol + 6).setCellValue(base);
-							iter.remove();
-							break;
+						if (Math.abs(shellVolume - volume) > volEps) {
+							continue;
+						}
+						double delta = Math.abs(coeff * shellPrice - price);
+						if (delta < minDelta) {
+							minDelta = delta;
+							minIndex = j;
 						}
 					}
+				}
+				
+				// found entry with closest price for same volume
+				if (minIndex != -1) {
+					Settlement sett = settlements.get(minIndex);
+					row.getCell(bblCol + 4).setCellValue(sett.getVolume());
+					row.getCell(bblCol + 5).setCellValue(sett.getSettleAmount() * coeff);
+					double base = sett.getSettleAmount() * coeff / sett.getVolume();
+					row.getCell(bblCol + 6).setCellValue(base);
+					mapped[minIndex] = true;
 				}
 	    	}
 			
 			// skip the summary line
 			rowId += 30;
 			// If not all matched, append leftover at the bottom
-			if (settlements.size() > 0) {
-				for (Settlement sett : settlements) {
-					Row row = sheet.createRow(rowId++);
-					Cell cell = row.createCell(bblCol + 4);
-					cell.setCellValue(sett.getVolume());
-					cell = row.createCell(bblCol + 5);
-					cell.setCellValue(sett.getSettleAmount() * coeff);
-					cell = row.createCell(bblCol + 6);
-					cell.setCellValue(sett.getSettleAmount() * coeff / sett.getVolume());
-				}
+			for (int j = 0; j < settlements.size(); j++) {
+				if (mapped[j] == true) continue;
+				Settlement sett = settlements.get(j);
+				Row row = sheet.createRow(rowId++);
+				Cell cell = row.createCell(bblCol + 4);
+				cell.setCellValue(sett.getVolume());
+				cell = row.createCell(bblCol + 5);
+				cell.setCellValue(sett.getSettleAmount() * coeff);
+				cell = row.createCell(bblCol + 6);
+				cell.setCellValue(sett.getSettleAmount() * coeff / sett.getVolume());
 			}
 		}
 		input.close();
@@ -113,6 +132,7 @@ public class SunocoXls {
         outFile.close();
 	}
 
+	
 	private static Map<Integer, Boolean> getVolMap(XSSFSheet sheet, int volCol) {
 		Map<String, Integer> countMap = new HashMap<String, Integer>();
 		int lastRow = sheet.getLastRowNum();
@@ -135,7 +155,7 @@ public class SunocoXls {
 			}
 		}
 		
-		// find duplicates
+		// find duplicates true for singlet, false for duplicates
 		Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
 		for (int i = 6; i < lastRow; i++) {
 			Row row = sheet.getRow(i);
